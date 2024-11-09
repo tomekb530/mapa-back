95a325bac795d477ea1c7b019cac1f84b1266491
kondi918 <kondex918@onet.pl>
2024-11-09 10:49:32 +0100

    feat: databaseCon
    Added database connection

---
+++ DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mapa_back
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
+using DotNetEnv;
+using mapa_back;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+Env.Load();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
+
+app.UseCors(options => 
+{
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+    options.AllowAnyOrigin();
+ });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
