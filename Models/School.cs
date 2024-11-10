c272da6a30460347ef81e5441039db5e5285cdcd
kondi918 <kondex918@onet.pl>
2024-11-10 14:53:02 +0100

    feat: schoolsModel
    Added schoolsModel

---
+++ Models/School.cs
@@ -0,0 +1,10 @@
+﻿using mapa_back.Models.RSPOApi;
+
+namespace mapa_back.Models
+{
+    public class School
+    {
+        public string BusinessData { get; set; }
+        public Geolokalizacja Geometry { get; set; } 
+    }
+}
